af28763d23c248e46eca7df0f7aeb93d70b89059
Табеев Александр <tabeev2017gmail.com@yandex.ru>
2024-12-20 18:49:44 +0400

    lab 6

---
+++ lib/components/resources.g.dart
@@ -0,0 +1,12 @@
+/// Generate by [flutter_assets_generator](https://github.com/goodswifter/flutter_assets_generator) library.
+///
+/// PLEASE DO NOT EDIT MANUALLY.
+class R {
+  const R._();
+
+  /// ![preview](file://C:\Users\tabee\StudioProjects\pmu_labss\assets\svg\ru.svg)
+  static const String assetsSvgRuSvg = 'assets/svg/ru.svg';
+
+  /// ![preview](file://C:\Users\tabee\StudioProjects\pmu_labss\assets\svg\uk.svg)
+  static const String assetsSvgUkSvg = 'assets/svg/uk.svg';
+}
